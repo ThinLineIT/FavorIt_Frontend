d3a66e76f835738261ea506e754628bb56682c75
createhb21 <alsghk9701@gmail.com>
2022-06-20 15:28:56 +0900

    feat: Add Pagination Components in Create Page #9

--- pages/fund/create.tsx
+++ pages/fund/create.tsx
@@ -6,7 +6,8 @@ import React, { useEffect } from 'react';
 import { GoBack } from '@components/layout';
 import { GeneratorType, isLocalGenerator } from '@recoil/create';
 import styled from '@emotion/styled';
-import { flexbox } from '@styles/mixins/_flexbox';
+import { columnFlexbox, flexbox } from '@styles/mixins/_flexbox';
+import { posCenter, posCenterX } from '@styles/mixins/_positions';
 
 // @Note 추후 분리하기
 const Crawling = dynamic(
@@ -29,10 +30,7 @@ const Description = dynamic(
   () => import('@components/domain/create/page-components/Description'),
   { ssr: false },
 );
-const RecipientName = dynamic(
-  () => import('@components/domain/create/page-components/RecipientName'),
-  { ssr: false },
-);
+
 const Dates = dynamic(
   () => import('@components/domain/create/page-components/Dates'),
   { ssr: false },
@@ -48,17 +46,46 @@ const hocComponents = [
   { page: 2, component: <Price /> },
   { page: 3, component: <Title /> },
   { page: 4, component: <Description /> },
-  { page: 5, component: <RecipientName /> },
-  { page: 6, component: <Dates /> },
-  { page: 7, component: <Preview /> },
+  { page: 5, component: <Dates /> },
+  { page: 6, component: <Preview /> },
 ];
 
 const Base = styled.div`
   width: 100%;
   height: 100%;
   padding: 0 20px;
-  margin-top: 100px;
-  ${flexbox('center', 'start')};
+  margin-top: 50px;
+  ${columnFlexbox('start', 'center')};
+`;
+
+const Pagination = styled.div`
+  width: 100%;
+  ${flexbox()};
+  flex-shrink: 1;
+  flex-grow: 0;
+  margin-bottom: 50px;
+  column-gap: 26px;
+`;
+
+const Chapter = styled.div<{ active: boolean }>`
+  width: 22px;
+  height: 22px;
+  position: relative;
+  border: 1px solid #92d2ff;
+  border-radius: calc((22px + 22px) / 2);
+  background-color: ${({ active }) => (active ? '#92d2ff' : '#fff')};
+  transform: ${({ active }) => (active ? 'scale(1.4)' : '')};
+  transition: background-color 200ms ease-in-out, transform 200ms ease-in-out;
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: calc(50% + 11px);
+    width: 26px;
+    display: block;
+    border: 0.5px solid #92d2ff;
+  }
 `;
 
 const Generate = () => {
@@ -67,7 +94,7 @@ const Generate = () => {
 
   useEffect(() => {
     if (generator.done === true) {
-      router.replace('/fund/123');
+      router.replace('/funding/1');
     }
   }, [generator, router]);
 
@@ -77,6 +104,14 @@ const Generate = () => {
   // 이에 대해서는 hoc를 활용해보려고 생각중
   return (
     <>
+      <Base>
+        <Pagination>
+          {hocComponents.map((ctx, idx) => (
+            <Chapter key={idx} active={generator.page === idx} />
+          ))}
+        </Pagination>
+        {hocComponents[generator.page].component}
+      </Base>
       <GoBack
         currying={() => {
           generator.page > 0
@@ -87,7 +122,6 @@ const Generate = () => {
             : router.replace('/');
         }}
       />
-      <Base>{hocComponents[generator.page].component}</Base>
     </>
   );
 };
